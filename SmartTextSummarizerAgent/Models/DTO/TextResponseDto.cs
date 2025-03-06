906eb15651ce1e668246f496014a0e4774eb1fc9
Ikenna Umeh <kennaumeh@gmail.com>
2025-03-06 21:22:26 +0100

    Added text controller, dtos, interface and class services

---
+++ SmartTextSummarizerAgent/Models/DTO/TextResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SmartTextSummarizerAgent.Models.DTO
+{
+    public class TextResponseDto
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+}
